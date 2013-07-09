1db20b94674bbf8b540cc7b37c6ad84b918e1c0c
Robert Ikeda <rmikeda@cs.stanford.edu>
2013-07-08 23:15:41 -0700

    bug fix

--- web.js
+++ web.js
@@ -4,11 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
-  var retstr = buf.toString('ascii');
+  var buf = fs.readFileSync('index.html');
+  var retstr = buf.toString();
   response.send(retstr);
 });
 
