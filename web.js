4631e33502dd185d8617abfad7abf966d1cdf739
Ubuntu <ubuntu@ip-10-154-183-53.ec2.internal>
2013-07-09 06:07:19 +0000

    ascii

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
   if (err) throw err;
   console.log(data);
 });
-  var retstr = buf.toString('utf8');
+  var retstr = buf.toString('ascii');
   response.send(retstr);
 });
 
